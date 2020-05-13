b222f8f479756f9fa782af72cf11c033ec5c18a5
Kilderson Sena <dersonsena@gmail.com>
2020-05-13 17:56:18 -0300

    refactoring and creating the factory to sut

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,28 +1,32 @@
 const bcrypt = require('bcrypt')
 
-class Encrypter {
-  async compare (value, hash) {
-    const isValid = await bcrypt.compare(value, hash)
-    return isValid
+const makeSut = () => {
+  class Encrypter {
+    async compare (value, hash) {
+      const isValid = await bcrypt.compare(value, hash)
+      return isValid
+    }
   }
+
+  return new Encrypter()
 }
 
 describe('Encrypter', () => {
   test('Should return true if bcrypt return true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(true)
   })
 
   test('Should return false if bcrypt return true', async () => {
     bcrypt.isValid = false
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(false)
   })
 
   test('Should call bcrypt with correct values', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     await sut.compare('any_value', 'any_hash')
 
     expect(bcrypt.value).toBe('any_value')
