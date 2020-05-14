bbdc6ea188882bed75e1931a7d1cccd37d8b1110
Kilderson Sena <dersonsena@gmail.com>
2020-05-14 15:06:21 -0300

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,9 @@
 module.exports = app => {
   app.disable('x-powered-by')
+  app.use((request, response, next) => {
+    response.set('access-control-allow-origin', '*')
+    response.set('access-control-allow-methods', '*')
+    response.set('access-control-allow-headers', '*')
+    next()
+  })
 }
